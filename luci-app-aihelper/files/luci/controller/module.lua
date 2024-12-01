e909cc6f22b2be8d5020b54e77218690cbca0e8a
utakamo <utakamo.it@gmail.com>
2024-12-01 17:42:39 +0900

    Fix entry argument

--- luci-app-aihelper/files/luci/controller/module.lua
+++ luci-app-aihelper/files/luci/controller/module.lua
@@ -3,6 +3,6 @@ module("luci.controller.luci-app-aihelper.module", package.seeall)
 
 function index()
     entry({"admin", "network", "aihelper"}, firstchild(), "Aihelper", 30).dependent=false
-    entry({"admin", "network", "aihelper", "network"}, cbi("aihelper/chat"), "Chat with AI", 30).dependent=false
-    entry({"admin", "network", "aihelper", "network"}, cbi("aihelper/setting"), "AI Setting", 30).dependent=false
+    entry({"admin", "network", "aihelper", "chat"}, cbi("aihelper/chat"), "Chat with AI", 30).dependent=false
+    entry({"admin", "network", "aihelper", "setting"}, cbi("aihelper/setting"), "AI Setting", 30).dependent=false
 end
