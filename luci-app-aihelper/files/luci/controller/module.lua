71f713e3419c757ef52650f800b28df814cb063b
utakamo <utakamo.it@gmail.com>
2024-12-09 06:45:34 +0900

    Change page title (AI Setting ---> Setting)

--- luci-app-aihelper/files/luci/controller/module.lua
+++ luci-app-aihelper/files/luci/controller/module.lua
@@ -4,5 +4,5 @@ module("luci.controller.luci-app-aihelper.module", package.seeall)
 function index()
     entry({"admin", "network", "aihelper"}, firstchild(), "Aihelper", 30).dependent=false
     entry({"admin", "network", "aihelper", "chat"}, template("luci-app-aihelper/chat"), "Chat with AI", 30).dependent=false
-    entry({"admin", "network", "aihelper", "setting"}, cbi("luci-app-aihelper/setting"), "AI Setting", 30).dependent=false
+    entry({"admin", "network", "aihelper", "setting"}, cbi("luci-app-aihelper/setting"), "Setting", 30).dependent=false
 end
