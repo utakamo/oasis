96e7965d469e559e03d33a01faff80c0e7340016
utakamo <utakamo.it@gmail.com>
2024-12-13 07:21:39 +0900

    Update chat list ui

--- luci-app-oasis/files/luci/controller/module.lua
+++ luci-app-oasis/files/luci/controller/module.lua
@@ -3,6 +3,18 @@ module("luci.controller.luci-app-oasis.module", package.seeall)
 
 function index()
     entry({"admin", "network", "oasis"}, firstchild(), "Oasis", 30).dependent=false
-    entry({"admin", "network", "oasis", "chat"}, template("luci-app-oasis/chat"), "Chat with AI", 30).dependent=false
     entry({"admin", "network", "oasis", "setting"}, cbi("luci-app-oasis/setting"), "Setting", 30).dependent=false
+    entry({"admin", "network", "oasis", "chat"}, template("luci-app-oasis/chat"), "Chat with AI", 30).dependent=false
+    entry({"admin", "network", "oasis", "chat-list"}, call("retrive_chat_list"), nil).leaf = true
+end
+
+function retrive_chat_list()
+    local util = require("luci.util")
+    local luci_http = require("luci.http")
+
+    -- ubus call
+    local result = util.ubus("oasis.chat", "list", {})
+
+    luci_http.prepare_content("application/json")
+    luci_http.write_json(result)
 end
