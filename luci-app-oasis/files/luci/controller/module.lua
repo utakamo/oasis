ffb84e9a6ee2183a4919e0dab003285679be6021
utakamo <utakamo.it@gmail.com>
2024-12-14 09:19:34 +0900

    Update chat list ui

--- luci-app-oasis/files/luci/controller/module.lua
+++ luci-app-oasis/files/luci/controller/module.lua
@@ -1,4 +1,5 @@
--- module.lua
+local util = require("luci.util")
+local luci_http = require("luci.http")
 module("luci.controller.luci-app-oasis.module", package.seeall)
 
 function index()
@@ -6,14 +7,31 @@ function index()
     entry({"admin", "network", "oasis", "setting"}, cbi("luci-app-oasis/setting"), "Setting", 30).dependent=false
     entry({"admin", "network", "oasis", "chat"}, template("luci-app-oasis/chat"), "Chat with AI", 30).dependent=false
     entry({"admin", "network", "oasis", "chat-list"}, call("retrive_chat_list"), nil).leaf = true
+    entry({"admin", "network", "oasis", "load-chat-data"}, call("load_chat_data"), nil).leaf = true
 end
 
 function retrive_chat_list()
-    local util = require("luci.util")
-    local luci_http = require("luci.http")
-
     -- ubus call
     local result = util.ubus("oasis.chat", "list", {})
+
+    luci_http.prepare_content("application/json")
+    luci_http.write_json(result)
+end
+
+function load_chat_data()
+
+    local params = luci_http.formvalue("params")
+
+    if not params then
+        luci_http.prepare_content("application/json")
+        luci_http.write_json({ error = "Missing params" })
+        return
+    end
+
+    -- Create a parameter table for ubus call
+    local json_param = { id = params }
+
+    local result = util.ubus("oasis.chat", "load", json_param)
 
     luci_http.prepare_content("application/json")
     luci_http.write_json(result)
