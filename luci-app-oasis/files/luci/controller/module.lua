e3656d29c8d699f92b07c22cc1737104cb8cced6
utakamo <utakamo.it@gmail.com>
2025-01-07 07:53:16 +0900

    Add webapi(apply and confirm)

--- luci-app-oasis/files/luci/controller/module.lua
+++ luci-app-oasis/files/luci/controller/module.lua
@@ -1,5 +1,8 @@
 local util = require("luci.util")
 local luci_http = require("luci.http")
+local jsonc = require("luci.jsonc")
+local oasis = require("oasis.chat.apply")
+
 module("luci.controller.luci-app-oasis.module", package.seeall)
 
 function index()
@@ -11,6 +14,8 @@ function index()
     entry({"admin", "network", "oasis", "export-chat-data"}, call("load_chat_data"), nil).leaf = true
     entry({"admin", "network", "oasis", "delete-chat-data"}, call("delete_chat_data"), nil).leaf = true
     entry({"admin", "network", "oasis", "rename-chat"}, call("rename"), nil).leaf = true
+    entry({"admin", "network", "oasis", "apply-uci-cmd"}, call("apply_uci_cmd"), nil).leaf = true
+    entry({"admin", "network", "oasis", "confirm"}, call("confirm"), nil).leaf = true
 end
 
 function retrive_chat_list()
@@ -76,4 +81,45 @@ function rename()
 
     luci_http.prepare_content("application/json")
     luci_http.write_json(result)
+end
+
+function apply_uci_cmd()
+
+    local uci_list_json = luci_http.formvalue("uci_list")
+    local chat_id = luci_http.formvalue("id")
+
+    if (not uci_list_json) or (not chat_id) then
+        -- for debug
+        os.execute("echo \"apply_uci_cmd argument error\" >> /tmp/oasis-apply.log")
+        luci_http.prepare_content("application/json")
+        luci_http.write_json({ error = "Missing params" })
+        return
+    end
+
+    -- for debug
+    os.execute("echo \"chat_id = " .. chat_id .. "\" >> /tmp/oasis-apply.log")
+    os.execute("echo \"uci_list_json" .. uci_list_json .. "\" >> /tmp/oasis-apply.log")
+
+    local uci_list = jsonc.parse(uci_list_json)
+
+    oasis.apply(uci_list, chat_id)
+
+    luci_http.prepare_content("application/json")
+    luci_http.write_json("OK")
+end
+
+function confirm()
+    local answer = luci_http.formvalue("answer")
+
+    if not answer then
+        os.execute("echo \"confirm error\" >> /tmp/oasis-confirm.log")
+        luci_http.prepare_content("application/json")
+        luci_http.write_json({ error = "Missing params" })
+        return
+    end
+
+    local result = oasis.confirm(answer)
+
+    luci_http.prepare_content("application/json")
+    luci_http.write_json("OK")
 end
