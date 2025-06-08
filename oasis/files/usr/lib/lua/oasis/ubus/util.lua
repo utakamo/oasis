bf0ba60d00d45e971e8cd5495dc83fb3f820011a
utakamo <utakamo.it@gmail.com>
2025-06-08 17:48:14 +0900

    Update oasis ubus object

--- oasis/files/usr/lib/lua/oasis/ubus/util.lua
+++ oasis/files/usr/lib/lua/oasis/ubus/util.lua
@@ -13,6 +13,93 @@ local retrieve_config = function()
     return storage_json
 end
 
+local retrieve_icon_info = function(path, format)
+        local data = common.load_conf_file(path)
+
+        if (not data) or (not data.icons.path) or (not data.icons.using) then
+            if (format) and (format == "json") then
+                return jsonc.stringify({ status = common.status.not_found })
+            end
+
+            return { status = common.status.not_found }
+        end
+
+        local icon_tbl = {}
+        icon_tbl.ctrl = {}
+        icon_tbl.ctrl.path = data.icons.path
+        icon_tbl.ctrl.using = data.icons.using
+
+        icon_tbl.list = {}
+
+        for key, img_name in pairs(data.icons) do
+            if key:match("^icon_%d+$") then
+                icon_tbl.list[key] = img_name
+            end
+        end
+
+        local icon_json = jsonc.stringify(icon_tbl)
+
+        if (format) and (format == "json")  then
+            return icon_json
+        end
+
+        return icon_tbl
+end
+
+local retrieve_sysmsg = function(path, format)
+    local sysmsg_tbl = common.load_conf_file(path)
+
+    if not sysmsg_tbl then
+        if (format) and (format == "json")  then
+            return jsonc.stringify({ status = common.status.not_found })
+        end
+
+        return { status = common.status.not_found }
+    end
+
+    local sysmsg_json = jsonc.stringify(sysmsg_tbl)
+
+    if (format) and (format == "json") then
+        return sysmsg_json
+    end
+
+    return sysmsg_tbl
+end
+
+local retrieve_sysmsg_info = function(path, format)
+    local data_tbl = common.load_conf_file(path)
+
+    if not data_tbl then
+        if (format) and (format == "json")  then
+            return jsonc.stringify({ status = common.status.not_found })
+        end
+
+        return { status = common.status.not_found }
+    end
+
+    local icon_info_tbl = {}
+    icon_info_tbl.key = {}
+    icon_info_tbl.title = {}
+
+    for key, tbl in pairs(data_tbl) do
+        if tbl.title then
+            icon_info_tbl.key[#icon_info_tbl.key + 1] = key
+            icon_info_tbl.title[#icon_info_tbl.title + 1] = tbl.title
+        end
+    end
+
+    local icon_info_json = jsonc.stringify(icon_info_tbl)
+
+    if (format) and (format == "json")  then
+        return icon_info_json
+    end
+
+    return icon_info_tbl
+end
+
 return {
-    retrieve_config = retrieve_config,
+    retrieve_config         = retrieve_config,
+    retrieve_icon_info      = retrieve_icon_info,
+    retrieve_sysmsg         = retrieve_sysmsg,
+    retrieve_sysmsg_info    = retrieve_sysmsg_info,
 }
