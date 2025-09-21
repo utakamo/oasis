748d69b7cae1c210cb1ff21b506c80730e8e7426
utakamo <utakamo.it@gmail.com>
2025-09-21 10:36:43 +0900

    Update installer script

--- oasis_installer.sh
+++ oasis_installer.sh
@@ -4,13 +4,13 @@ set -e  # Exit immediately if a command fails
 opkg update
 
 # Download packages to /tmp
-wget -O /tmp/oasis_3.1.2-r1_all.ipk https://github.com/utakamo/oasis/releases/download/v3.1.2/oasis_3.1.2-r1_all.ipk
-wget -O /tmp/luci-app-oasis_3.1.2-r1_all.ipk https://github.com/utakamo/oasis/releases/download/v3.1.2/luci-app-oasis_3.1.2-r1_all.ipk
-wget -O /tmp/oasis-mod-tool_1.0.4-r1_all.ipk https://github.com/utakamo/oasis/releases/download/v3.1.2/oasis-mod-tool_1.0.4-r1_all.ipk
+wget -O /tmp/oasis_3.2.0-r1_all.ipk https://github.com/utakamo/oasis/releases/download/v3.2.0/oasis_3.2.0-r1_all.ipk
+wget -O /tmp/luci-app-oasis_3.2.0-r1_all.ipk https://github.com/utakamo/oasis/releases/download/v3.2.0/luci-app-oasis_3.2.0-r1_all.ipk
+wget -O /tmp/oasis-mod-tool_1.0.4-r1_all.ipk https://github.com/utakamo/oasis/releases/download/v3.2.0/oasis-mod-tool_1.0.4-r1_all.ipk
 
 # Install packages from /tmp
-opkg install /tmp/oasis_3.1.2-r1_all.ipk
-opkg install /tmp/luci-app-oasis_3.1.2-r1_all.ipk
+opkg install /tmp/oasis_3.2.0-r1_all.ipk
+opkg install /tmp/luci-app-oasis_3.2.0-r1_all.ipk
 opkg install /tmp/oasis-mod-tool_1.0.4-r1_all.ipk
 
 # Verify installation
@@ -18,8 +18,8 @@ echo "Installed packages:"
 opkg list-installed | grep -E 'oasis|luci-app-oasis|oasis-mod-tool'
 
 # Clean up temporary files if no longer needed
-rm -f /tmp/oasis_3.1.2-r1_all.ipk \
-      /tmp/luci-app-oasis_3.1.2-r1_all.ipk \
+rm -f /tmp/oasis_3.2.0-r1_all.ipk \
+      /tmp/luci-app-oasis_3.2.0-r1_all.ipk \
       /tmp/oasis-mod-tool_1.0.4-r1_all.ipk
 
 # Completion message
