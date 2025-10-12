62b24c77526ff0f59047abb2c5977aa88a19d4db
utakamo <utakamo.it@gmail.com>
2025-10-12 13:54:16 +0900

    Update installer script

--- oasis_installer.sh
+++ oasis_installer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e  # Exit immediately if a command fails
 
-OASIS_VER="${OASIS_VER:-3.2.4-r1}"
-LUCI_VER="${LUCI_VER:-3.2.4-r1}"
-TOOL_VER="${TOOL_VER:-1.1.0-r1}"
+OASIS_VER="${OASIS_VER:-3.2.6-r1}"
+LUCI_VER="${LUCI_VER:-3.2.6-r1}"
+TOOL_VER="${TOOL_VER:-1.1.1-r1}"
 RELEASE_TAG="${RELEASE_TAG:-v3.2.4}"
 
 BASE_URL="https://github.com/utakamo/oasis/releases/download/${RELEASE_TAG}"
